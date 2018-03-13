e597b929440fc82157b45cb4faddff132dc89d23
Hemesh-Unka <hemeshunka@gmail.com>
2018-03-13 11:29:40 +0000

    Updated connection code

--- lib/link.rb
+++ lib/link.rb
@@ -2,6 +2,8 @@ require 'pg'
 
 class Link
   def self.all
-    connection = PG.connect :dbname => 'bookmark_manager', :user => 'hemesh'
+    connection = PG.connect(dbname: 'bookmark_manager')
+    result = connection.exec('SELECT * FROM links')
+    result.map { |link| link['url'] }
   end
 end
